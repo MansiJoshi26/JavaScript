030c6b9a81e2d0ee7f6c5a290ec255ba6d8181da
Mansi Joshi <mjmansi2620@gmail.com>
2025-07-11 10:00:31 +0530

    Add files via upload
    Practice Question

---
+++ JavaScript/t1.js
@@ -0,0 +1,19 @@
+
+/*let cart=[{
+
+    id:1,name:"Mic"
+
+}];
+function AddCart(cart,item){
+
+    cart.push(item);
+    return cart;
+
+}
+let item={id:3,name:"Mansi"}
+AddCart(cart,item);
+console.log(cart);
+function Remove(cart,itemName){
+    cart.filter(e=>e.name !=itemName);
+
+}*/
